5ad46a1f2a8afd05d7e71aa298e36eec49a63ab2
Alex-0293 <1928311@tuta.io>
2020-04-18 19:40:00 +0300

    Add SCRIPT and VARS folders. Change structure.

--- SCRIPTS/PassExpEmails.ps1
+++ SCRIPTS/PassExpEmails.ps1
@@ -40,10 +40,11 @@ trap {
 #########################################################################
 Clear-Host
 
-[string]$MyScriptRoot = Get-WorkDir
+[string]$MyScriptRoot       = Get-WorkDir
+[string]$Global:ProjectRoot = Split-Path $MyScriptRoot -parent
 
-Get-VarsFromFile    "$MyScriptRoot\Vars.ps1"
-Initialize-Logging $MyScriptRoot "Latest"
+Get-VarsFromFile    "$ProjectRoot\VARS\Vars.ps1"
+Initialize-Logging   $ProjectRoot "Latest"
 
 
 
